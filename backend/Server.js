90ba7adc9edade3c4a330a6f9b88640ab9261e96
Vedanth <vedanth8319@gmail.com>
2026-04-22 11:30:29 +0530

    Changed duration of rate limiting

--- backend/Server.js
+++ backend/Server.js
@@ -23,9 +23,9 @@ const app = express();
 app.use(helmet()); // Secure HTTP headers
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 1 * 60 * 1000, // 1 minute for testing
   max: 100, // Limit each IP to 100 requests per windowMs
-  message: "Too many requests from this IP, please try again after 15 minutes",
+  message: "Too many requests from this IP, please try again after 1 minute",
 });
 app.use("/api/", limiter); // Apply rate limiting to all API routes
 const server = http.createServer(app);
